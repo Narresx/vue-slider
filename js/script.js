630d972d7b5d030d51063936451b2948235db94f
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-27 17:26:20 +0100

    add images in sliders

--- js/script.js
+++ js/script.js
@@ -9,17 +9,19 @@ const root = new Vue({
     el: '#root',
 
     data: {
+
         images: {
             image1: 'images/image1.jpg',
             image2: 'images/image2.jpg',
             image3: 'images/image3.jpg',
             image4: 'images/image4.jpg',
         },
+        currentIndex: undefined,
+        index: 0,
     },
 
     methods: {
-        isActive() {
-
+        isActive() {  // Funzione che aggiunge la classe active al pallino corrente
         },
     },
 });
