52dfa33ee8d2c0d63ea8c7df658bd8db409a75dd
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-27 15:33:11 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('JS OK');
+
+console.log('Vue ok', Vue);
+
+const root = new Vue({
+
+    el: '#root',
+
+    data: {
+        images: {
+            image1: 'images/image1.jpg',
+            image2: 'images/image2.jpg',
+            image3: 'images/image3.jpg',
+            image4: 'images/image4.jpg',
+        },
+
+    },
+
+    methods: {
+
+    },
+})
